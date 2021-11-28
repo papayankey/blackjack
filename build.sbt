e26fca0eb37e74b05af023eccce1ab16f910a101
benneth-yankey <benneth.yankey@turntabl.io>
2021-11-28 03:34:58 +0000

    initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "blackjack"
+
+version := "0.1"
+
+scalaVersion := "3.1.0"
+
+idePackagePrefix := Some("com.papayankey.github")
---
+++ src/main/scala/Blackjack.scala
@@ -0,0 +1,7 @@
+package com.papayankey.github
+
+object Blackjack {
+  def main(args: Array[String]): Unit = {
+    println("Hello Scala!")
+  }
+}
