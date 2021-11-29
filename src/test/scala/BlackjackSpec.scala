005c54a30c2f257bb6618c8c3ce237f19cc69a9b
papayankey <yankeybenneth@gmail.com>
2021-11-29 01:48:43 +0000

    added test for initialize method

--- src/test/scala/BlackjackSpec.scala
+++ src/test/scala/BlackjackSpec.scala
@@ -2,12 +2,23 @@ package com.papayankey.github
 
 import org.scalatest.flatspec.AnyFlatSpec
 
+import scala.collection.mutable
+
 class BlackjackSpec extends AnyFlatSpec {
 
   "A card" should "be created given a suit and rank" in {
     val expected = s"Card(Diamond, Jack)"
     assertResult(expected) {
       Card(Suit.Diamond, Rank.Jack).toString
+    }
+  }
+
+  "A deck" should "be initialized with 52 combination of cards" in {
+    val expected = 52
+    assertResult(52) {
+      val deck = Deck(mutable.Stack[Card]())
+      deck.initialize
+      deck.cards.length
     }
   }
 
